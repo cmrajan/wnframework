80b7998beba5fcc00df58610821890d6c4db17d0
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-01-28 09:18:51 +0000

    hidden bug: test

--- v170/js/widgets/form/fields.js
+++ v170/js/widgets/form/fields.js
@@ -153,6 +153,7 @@ Field.prototype.refresh_display = function() {
 		}
 		this.set_status = this.disp_status;
 	}
+	if(this.df.fieldname=='area_list') alert(this.set_status);
 }
 
 Field.prototype.refresh = function() { 
