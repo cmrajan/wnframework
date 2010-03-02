f1e53adba8e9d2a9b59fdd211ad3213e59b10a12
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-03-02 12:32:39 +0000

    toolbars... IE Fixes

--- v170/js/widgets/select.js
+++ v170/js/widgets/select.js
@@ -11,12 +11,26 @@ function SelectWidget(parent, options, width, editable, bg_color) {
 		if(width) 
 			$y(this.wrapper, { width: width } );		
 
-		this.body_tab = make_table(this.wrapper, 1, 2,'100%', ['100%', '18px'],{border:'1px solid #AAA'});
-	
-		this.inp = $a_input($td(this.body_tab, 0, 0), 'text', {'readonly':(editable ? null : 'readonly')}, {width: '96%', border:'0px', padding:'1px'});
-		
-		this.btn = $a($td(this.body_tab, 0, 1), 'img', '', {cursor:'pointer', margin:'1px 2px'});
-		this.btn.src = 'images/ui/down-arrow.gif';
+		if(isIE) {
+			this.body_tab = make_table(this.wrapper, 1, 2,'100%', ['100%', '18px'],{border:'1px solid #AAA'});
+			this.inp = $a_input($td(this.body_tab, 0, 0), 'text', {'readonly':(editable ? null : 'readonly')}, {width: '96%', border:'0px', padding:'1px'});
+			this.btn = $a($td(this.body_tab, 0, 1), 'img', '', {cursor:'pointer', margin:'1px 2px'});
+			this.btn.src = 'images/ui/down-arrow.gif';
+		} else {
+			this.body_tab = make_table(this.wrapper, 1, 2,'100%', ['100%', '18px'], {verticalAlign:'middle'});	
+			this.inp = $a_input($td(this.body_tab, 0, 0), 'text', {'readonly':(editable ? null : 'readonly')}, {width: '96%', border:'1px solid #CCC', borderRight:'0px', padding:'2px', height:'14px'});
+			this.btn = $a($td(this.body_tab, 0, 1), 'button', '', {height:'20px', width:'18px', padding:'0px'});
+			$(this.btn)
+				.button({icons:{primary:'ui-icon-triangle-1-s'}})
+				.removeClass("ui-corner-all").addClass("ui-corner-right ui-button-icon")
+				.position({ my: "left center", at: "right center", of: this.inp, offset: "-1 0" });
+		}
+
+		if(!editable) {
+			$y(this.inp, {cursor:'pointer'});
+			this.inp.onclick = this.btn.onclick;
+		}
+
 		this.btn.onclick = function() {
 			if(me.as && me.as.body) {
 				me.as.clearSuggestions();
@@ -25,12 +39,6 @@ function SelectWidget(parent, options, width, editable, bg_color) {
 			me.inp.focus();
 			me.as.createList(me.as.aSug);
 		}
-
-		if(!editable) {
-			$y(this.inp, {cursor:'pointer'});
-			this.inp.onclick = this.btn.onclick;
-		}
-
 		this.as = new AutoSuggest(this.inp, {fixed_options: true, xdelta: 8, ydelta: 8});
 		this.as.aSug = me.create_options(me.options);
 		this.set_background();
@@ -46,9 +54,12 @@ function SelectWidget(parent, options, width, editable, bg_color) {
 	
 	this.set_background = function(color) {
 		if(color)this.bg_color = color;
-		$y($td(this.body_tab, 0, 0),{backgroundColor: this.bg_color});		
-		$y($td(this.body_tab, 0, 1),{backgroundColor:'#ECECDB'});		
-
+		$y(this.inp,{backgroundColor: this.bg_color});
+		if(isIE){
+			$y($td(this.body_tab, 0, 0),{backgroundColor: this.bg_color});
+			$y($td(this.body_tab, 0, 1),{backgroundColor:'#ECECDB'});
+		}	
+				
 	}
 	this.create_options = function(l) {
 		var o = [];
--- v170/js/wnf.compressed.js
+++ v170/js/wnf.compressed.js
@@ -567,13 +567,14 @@ this.fld.onchange();}};AutoSuggest.prototype.killTimeout=function(){cur_autosug=
 if(this.ul)
 delete this.ul;this.iHigh=0;if(this.oP.fixed_options&&this.fld.field_object&&this.fld.field_object.docname){var d=locals[this.fld.field_object.doctype][this.fld.field_object.docname];if(this.fld.fieldname){this.fld.value=d[this.fld.fieldname];}}};$ce=function(type,attr,cont,html)
 {var ne=document.createElement(type);if(!ne)return 0;for(var a in attr)ne[a]=attr[a];var t=typeof(cont);if(t=="string"&&!html)ne.appendChild(document.createTextNode(cont));else if(t=="string"&&html)ne.innerHTML=cont;else if(t=="object")ne.appendChild(cont);return ne;};function SelectWidget(parent,options,width,editable,bg_color){var me=this;this.bg_color=bg_color?bg_color:'#FFF';this.custom_select=1;this.setup=function(){this.options=options;this.wrapper=$a(parent,'div');if(width)
-$y(this.wrapper,{width:width});this.body_tab=make_table(this.wrapper,1,2,'100%',['100%','18px'],{border:'1px solid #AAA'});this.inp=$a_input($td(this.body_tab,0,0),'text',{'readonly':(editable?null:'readonly')},{width:'96%',border:'0px',padding:'1px'});this.btn=$a($td(this.body_tab,0,1),'img','',{cursor:'pointer',margin:'1px 2px'});this.btn.src='images/ui/down-arrow.gif';this.btn.onclick=function(){if(me.as&&me.as.body){me.as.clearSuggestions();return;}
-me.inp.focus();me.as.createList(me.as.aSug);}
+$y(this.wrapper,{width:width});if(isIE){this.body_tab=make_table(this.wrapper,1,2,'100%',['100%','18px'],{border:'1px solid #AAA'});this.inp=$a_input($td(this.body_tab,0,0),'text',{'readonly':(editable?null:'readonly')},{width:'96%',border:'0px',padding:'1px'});this.btn=$a($td(this.body_tab,0,1),'img','',{cursor:'pointer',margin:'1px 2px'});this.btn.src='images/ui/down-arrow.gif';}else{this.body_tab=make_table(this.wrapper,1,2,'100%',['100%','18px'],{verticalAlign:'middle'});this.inp=$a_input($td(this.body_tab,0,0),'text',{'readonly':(editable?null:'readonly')},{width:'96%',border:'1px solid #CCC',borderRight:'0px',padding:'2px',height:'14px'});this.btn=$a($td(this.body_tab,0,1),'button','',{height:'20px',width:'18px',padding:'0px'});$(this.btn).button({icons:{primary:'ui-icon-triangle-1-s'}}).removeClass("ui-corner-all").addClass("ui-corner-right ui-button-icon").position({my:"left center",at:"right center",of:this.inp,offset:"-1 0"});}
 if(!editable){$y(this.inp,{cursor:'pointer'});this.inp.onclick=this.btn.onclick;}
+this.btn.onclick=function(){if(me.as&&me.as.body){me.as.clearSuggestions();return;}
+me.inp.focus();me.as.createList(me.as.aSug);}
 this.as=new AutoSuggest(this.inp,{fixed_options:true,xdelta:8,ydelta:8});this.as.aSug=me.create_options(me.options);this.set_background();}
 this.set_width=function(w){w=cint(w);$y(this.inp,{width:(w-20)+'px'});$y(this.body_tab,{width:w+'px'});$y($td(this.body_tab,0,0),{width:(w-18)+'px'})
 $y($td(this.body_tab,0,1),{width:'18px'});}
-this.set_background=function(color){if(color)this.bg_color=color;$y($td(this.body_tab,0,0),{backgroundColor:this.bg_color});$y($td(this.body_tab,0,1),{backgroundColor:'#ECECDB'});}
+this.set_background=function(color){if(color)this.bg_color=color;$y(this.inp,{backgroundColor:this.bg_color});if(isIE){$y($td(this.body_tab,0,0),{backgroundColor:this.bg_color});$y($td(this.body_tab,0,1),{backgroundColor:'#ECECDB'});}}
 this.create_options=function(l){var o=[];for(var i=0;i<l.length;i++){o.push({value:l[i]});}
 return o;}
 this.set_options=function(l){this.as.init();this.as.aSug=me.create_options(l);}
