86bdd1a85fb1fb26ab4df68dc4796fe37cdf659c
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-03-11 10:14:09 +0000

    comments in table

--- v170/js/widgets/form/fields.js
+++ v170/js/widgets/form/fields.js
@@ -60,7 +60,7 @@ Field.prototype.set_comment = function() {
 	if(this.df.description) {
 		if(this.df.description.length > 100) {
 			$($a(this.comment_area, 'div')).html(this.df.description.substr(0,100) + '...');
-			$($a($a(this.comment_area, 'div'),'span'), 'link_type', {fontSize:'11px'})).html('more').click(function() { msgprint(me.df.description) });
+			$($a($a(this.comment_area, 'div'),'span', 'link_type', {fontSize:'11px'})).html('more').click(function() { msgprint(me.df.description) });
 		} else {
 			this.comment_area.innerHTML = replace_newlines(this.df.description);
 		}
