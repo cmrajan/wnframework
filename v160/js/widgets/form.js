f414fb6d6bc0a766427515885374cd8cc4a2a341
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-03-29 07:19:58 +0000

    add new lines in print format

--- v160/js/widgets/form.js
+++ v160/js/widgets/form.js
@@ -3329,6 +3329,7 @@ function print_makepage(body, style, doc, title) {
 		jslist = block.getElementsByTagName('script');
 	}
 	var main_html = block.innerHTML.replace(/<td>/g, '\n<td>');
+	if(user=='Administrator')alert(main_html);
 	return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n'
 		+ '<html><head>'
 		+'<title>'+title+'</title>'
