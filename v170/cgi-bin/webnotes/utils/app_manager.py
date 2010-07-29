1e031dcd56c23595dcb2fa22f24e78fbccf2353d
dalal.saumil <dalal.saumil@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-07-29 09:29:47 +0000

    syntax error solved

--- v170/cgi-bin/webnotes/utils/app_manager.py
+++ v170/cgi-bin/webnotes/utils/app_manager.py
@@ -61,7 +61,7 @@ class AppManager:
 	# ----------------------------------
 	def create_apps(self, n, source):
 		acc_conn = webnotes.db.Database(use_default=1)
-		curr_ac_name = sql("select ac_name from tabAccount where ac_name like 'ax%' Order by ac_name desc limit 1")
+		curr_ac_name = acc_conn.sql("select ac_name from tabAccount where ac_name like 'ax%' Order by ac_name desc limit 1")
 		curr_ac_name = curr_ac_name and curr_ac_name[0][0] or 0
 		if curr_ac_name:
 			curr_ac_name = cint(curr_ac_name[2:])
