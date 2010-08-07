d7f9ddae1b3967ecb4d1f4dbe08fbc61f082a389
jai.madhav86 <jai.madhav86@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-08-07 08:22:16 +0000

    sync of gl mapper, tds category and tds rate chart added

--- v170/cgi-bin/webnotes/utils/app_manager.py
+++ v170/cgi-bin/webnotes/utils/app_manager.py
@@ -127,6 +127,9 @@ class App:
 		self.sync_records('Print Format')
 		self.sync_records('DocType Mapper')
 		self.sync_records('DocType Label')
+		self.sync_records('GL Mapper')
+		self.sync_records('TDS Category')
+		self.sync_records('TDS Rate Chart')
 		self.sync_control_panel()
 		self.close()
 		
