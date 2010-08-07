2acd0766720f443da10244af7aa9a77f77de11e0
jai.madhav86 <jai.madhav86@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-08-07 08:25:49 +0000

    sync of gl mapper added

--- v170/cgi-bin/webnotes/utils/app_manager.py
+++ v170/cgi-bin/webnotes/utils/app_manager.py
@@ -128,8 +128,6 @@ class App:
 		self.sync_records('DocType Mapper')
 		self.sync_records('DocType Label')
 		self.sync_records('GL Mapper')
-		self.sync_records('TDS Category')
-		self.sync_records('TDS Rate Chart')
 		self.sync_control_panel()
 		self.close()
 		
