b98caade39606cedc970e89227787beef87ef717
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-08-13 11:54:35 +0000

    Server transfer.py import doclist

--- v170/cgi-bin/webnotes/utils/transfer.py
+++ v170/cgi-bin/webnotes/utils/transfer.py
@@ -181,6 +181,7 @@ def ovr_doctype(doc_list, ovr, ignore, onupdate):
 	import webnotes
 	from webnotes.model.doc import Document
 	from webnotes.model import doclist
+	from webnotes.utils import cint
 	doc_list = [Document(fielddata = d) for d in doc_list]
 	doc = doc_list[0]
 	orig_modified = doc.modified
