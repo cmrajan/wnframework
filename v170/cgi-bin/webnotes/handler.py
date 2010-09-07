c48542d440c63b2daf056b0fd271b0ac84b201b1
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-09-07 09:59:07 +0000

    allow get page for guest

--- v170/cgi-bin/webnotes/handler.py
+++ v170/cgi-bin/webnotes/handler.py
@@ -372,7 +372,7 @@ else:
 
 			# load module
 			if webnotes.session['user'] == 'Guest':
-				if cmd not in ['runserverobj', 'webnotes.widgets.form.getdoc','webnotes.widgets.form.getdoc','logout']:
+				if cmd not in ['runserverobj', 'webnotes.widgets.form.getdoc','webnotes.widgets.form.getdoctype','logout','webnotes.widgets.page.getpage']:
 					webnotes.msgprint('Guest not allowed to perform this action')
 					raise Exception
 
