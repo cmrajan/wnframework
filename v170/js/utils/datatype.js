7ad57f52dd426307094066693d028acfee8be431
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-09-07 12:15:09 +0000

    added in_words in datatype.js

--- v170/js/utils/datatype.js
+++ v170/js/utils/datatype.js
@@ -229,9 +229,41 @@ function DocLink(p, doctype, name, onload) {
 var doc_link = DocLink;
 
 // in words
+
+var known_numbers = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+}
+
 function in_words(n) {
     n=cint(n)
-    if(known[n]) return known[n];
+    if(known_numbers[n]) return known_numbers[n];
     var bestguess = n + '';
     var remainder = 0
     if(n<=20)
