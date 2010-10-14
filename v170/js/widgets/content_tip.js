a49fa88c2e2b8e71fbdb56d23a38c1aa9d0cee0e
yogesh12585 <yogesh12585@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-10-14 06:32:18 +0000

    IE fix in content_tip.js for transparent border

--- v170/js/widgets/content_tip.js
+++ v170/js/widgets/content_tip.js
@@ -2,13 +2,13 @@
 ContentTip = function(parent, tip_content, cs){
 
   $y(parent,{position:'relative', cursor:'pointer'});
-  
+ 
   this.parent = parent;
   this.tip_content = tip_content;
   if(cs) this.cs = cs;
-  
+ 
   this.make();
-  
+ 
   this.onhover();
 }
 
@@ -18,7 +18,7 @@ ContentTip.prototype.make = function(){
   // Tip body wrapper
   this.wrapper = $a(null,'div','tip', {paddingBottom:'8px'});
   this.body = $a(this.wrapper,'div','tip_body');
-  
+ 
   // arrow container
   this.arrow_wrapper = $a(null,'div','tip_arrow_container');
   this.arrow_main = $a(this.arrow_wrapper,'div','tip_arrow main');
@@ -26,9 +26,8 @@ ContentTip.prototype.make = function(){
 
   // set parent
   this.parent.tip = this.wrapper;
-  
-  this.set_style()
-  
+ 
+  this.set_style();
   this.set_param();
 }
 
@@ -37,11 +36,11 @@ ContentTip.prototype.set_param = function(){
 
   // set tip width
   $(this.wrapper).appendTo('body');
-  
+ 
   var tmp = $a(null,'div');
   tmp.innerHTML = this.tip_content;
   this.body.appendChild(tmp);
-  
+ 
   this.parent.width = $(this.wrapper).outerWidth() + 4*2;
 
 	this.set_parent();
@@ -51,7 +50,7 @@ ContentTip.prototype.set_param = function(){
 ContentTip.prototype.set_parent = function(){
 
   $(this.wrapper).remove();
-  
+ 
   // set tip
   $(this.wrapper).appendTo(this.parent);
   $(this.arrow_wrapper).appendTo(this.body);
@@ -59,22 +58,22 @@ ContentTip.prototype.set_parent = function(){
 
 // onhover
 ContentTip.prototype.onhover = function(){
-		
+
   // set hover
 	$(this.parent).hover(
-  
+
 		function(){
-      $y(this.tip, {width:this.width + 'px'});    
-      var top = - ($(this.tip).outerHeight());
-      
-			$(this.tip)
-				.css('top', top + 'px')
-				.css('display', 'block')
-		},
-		
-		function(e){
-      $(this.tip).css('display','none');
-		}
+			$y(this.tip, {width:this.width + 'px'});    
+			var top = - ($(this.tip).outerHeight());
+
+      $(this.tip)
+	      .css('top', top + 'px')
+        .css('display', 'block')
+    },
+		     
+    function(e){
+			$(this.tip).css('display','none');
+    }
 	);
 }
 
@@ -85,17 +84,15 @@ ContentTip.prototype.set_style = function(){
 	if(this.cs){
 		for(d in this.cs){
 			if(d == 'backgroundColor'){
-			  //$y(this.body,{backgroundColor:this.cs[d]});
-			  //$y(this.arrow_main,{borderTop:'10px solid ' + this.cs[d]});
 				this.body.style[d] = this.cs[d];
 				this.arrow_main.style['borderTop'] = '14px solid ' + this.cs[d];
 			}
-			else if(!d.match(/border+/g)){
-				this.body.style[d] = this.cs[d];
-			}
-		}	
+		  else if(!d.match(/border+/g)){
+		  	this.body.style[d] = this.cs[d];
+		  }
+		}      
 	}
-	
+
 	this.set_border(4);
 }
 
@@ -103,18 +100,22 @@ ContentTip.prototype.set_style = function(){
 // set border
 ContentTip.prototype.set_border = function(border_size, border_color){
 
-  var bs = border_size ? border_size : 0;
-  var bc = border_color ? border_color : '#444';
+  var b_size = border_size ? border_size : 0;
+  var b_color = border_color ? border_color : '#444';
+	
+	// IE fix for transparent border
+	var b_style = isIE ? ' dotted ' : ' transparent ';
 
-  $y(this.body, {border: bs + 'px' + ' solid ' + bc});
-  
-  if(bs){
-    $y(this.arrow_border,{borderTop: bs + 14 + 'px' + ' solid ' + bc});
-    $y(this.arrow_border,{borderLeft: (bs + 14) / 2 + 'px' + ' transparent ' + bc});
-    $y(this.arrow_border,{borderRight: (bs + 14) / 2 + 'px' + ' transparent ' + bc});
-    $y(this.arrow_border,{top:bs + 'px'});
+  $y(this.body, {border: b_size + 'px' + ' solid ' + b_color});
+
+  if(b_size){
+    $y(this.arrow_border,{borderTop: b_size + 14 + 'px' + ' solid ' + b_color});
+		$y(this.arrow_border,{borderLeft: (b_size + 14) / 2 + 'px' + b_style + b_color});
+    $y(this.arrow_border,{borderRight: (b_size + 14) / 2 + 'px' + b_style + b_color});
+
+    $y(this.arrow_border,{top:b_size + 'px'});
   }
   else{
-    $y(this.arrow_border,{border: '0px' + ' solid ' + bc});
+    $y(this.arrow_border,{border: '0px' + ' solid ' + b_color});
   }
 }
