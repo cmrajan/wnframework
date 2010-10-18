cd2d723df67bb927f6ec8b46ca70f4653f3fad54
dalal.saumil <dalal.saumil@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-10-18 03:42:55 +0000

    server: import_docs.py check for naming series

--- v170/cgi-bin/webnotes/model/import_docs.py
+++ v170/cgi-bin/webnotes/model/import_docs.py
@@ -270,8 +270,8 @@ class CSVImport:
 							if f!='naming_series':
 								fd[f] = self.check_select_link_data(row, col, f, d[i], s= 'Select')
 							else:
-								exists = sql("select name from `tabNaming Series Options` where doc_type = %s and series_options = %s", (self.dt_list[0], d[i]))
-								if exists and cstr(exists[0][0]):
+								exists = sql("select name from `tabNaming Series Options` where doc_type = %s", (self.dt_list[0]))
+								if exists and cstr(exists[0][0]) and d[i] in cstr(exists[0][0]).split(NEWLINE):
 									fd[f] = d[i]
 								else:
 									self.msg.append('<div style="color: RED">At Row %s and Column %s : => Data "%s" in field [Series] Not Found in Naming Series</div>\n' % (row, col, d[i]))
