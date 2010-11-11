bf69af2417fbd7efaaea17ba6364fe37ec7ed3c0
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2010-11-11 07:32:59 +0000

    File system patch - now all files go to a folder

--- v170/cgi-bin/webnotes/utils/file_manager.py
+++ v170/cgi-bin/webnotes/utils/file_manager.py
@@ -120,10 +120,10 @@ def convert_to_files(verbose=0):
 	import webnotes
 	
 	# nfiles
-	fl = sql("select name from `tabFile Data`")
+	fl = webnotes.conn.sql("select name from `tabFile Data`")
 	for f in fl:
 		# get the blob
-		blob = sql("select blob_content from `tabFile Data` where name=%s" % f[0])[0][0]
+		blob = webnotes.conn.sql("select blob_content from `tabFile Data` where name=%s" % f[0])[0][0]
 		
 		if blob:
 			# write the file
