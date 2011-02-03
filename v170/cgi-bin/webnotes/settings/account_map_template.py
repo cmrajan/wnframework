d8e98859bbfa3d3702e56579c27961ad9a943bec
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-02-03 05:10:09 +0000

    added account map template as account_map should not be in svn

---
+++ v170/cgi-bin/webnotes/settings/account_map_template.py
@@ -0,0 +1,10 @@
+# Account/Domain Name to Database Mapping file
+# --------------------------------------------
+# last updated on: 2011-02-02 14:31:14
+
+default_db_name = "db_name"
+
+ac_name_map = {'main_instance':'my_db'}
+
+# without www
+domain_name_map = {'example.com': 'my_db'}
