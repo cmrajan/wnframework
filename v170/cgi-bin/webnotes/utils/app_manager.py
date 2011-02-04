a59ed41148e2116b47b1fbf495c347352be5769e
nabinhait <nabinhait@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-02-04 12:40:21 +0000

    changed path of install

--- v170/cgi-bin/webnotes/utils/app_manager.py
+++ v170/cgi-bin/webnotes/utils/app_manager.py
@@ -164,7 +164,7 @@ class AppManager:
 	# create a new app
 	# ----------------------------------
 	def new_app(self, ac_name, source):
-		from webnotes import install
+		import install
 
 		# setup
 		print 'Creating new application...'
