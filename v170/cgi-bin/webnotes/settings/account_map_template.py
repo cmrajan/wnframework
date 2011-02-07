0bb43f60b89dd5464413e05612c1c9f921de3b62
anand.jeyahar <anand.jeyahar@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-02-07 06:24:52 +0000

    Trunk: added fields specific to backup

--- v170/cgi-bin/webnotes/settings/account_map_template.py
+++ v170/cgi-bin/webnotes/settings/account_map_template.py
@@ -8,3 +8,19 @@ ac_name_map = {'main_instance':'my_db'}
 
 # without www
 domain_name_map = {'example.com': 'my_db'}
+
+#deleted status
+ac_deleted_map = {'':''}
+
+#last backup time
+last_backup_map = {'':''}
+
+#allocated status
+allocated_map = {'':''}
+
+#trial_or_paid status
+trial_or_paid_map = {'':''}
+
+#expiry_date_status
+expiry_date_map = {'':''}
+
