7b2962c53c94d2b4e6e9b743d700422265eee846
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-03-15 11:50:13 +0000

    webservice.py fixes

--- v170/cgi-bin/webnotes/utils/webservice.py
+++ v170/cgi-bin/webnotes/utils/webservice.py
@@ -64,7 +64,7 @@ class FrameworkServer:
 		protocol = self.https and 'https://' or 'http://'
 		req = urllib2.Request(protocol + os.path.join(self.remote_host, self.path, 'index.cgi'), urllib.urlencode(args))
 		for key in self.cookies:
-			req.add_header('Set-Cookie', '%s=%s' % (key, self.cookies[key]))
+			req.add_header('cookie', '%s=%s' % (key, self.cookies[key]))
 		return urllib2.urlopen(req)
 
 		"""
