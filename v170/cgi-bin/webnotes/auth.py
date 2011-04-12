24b215931eb16c15e5324ea5ae9bf7834e914586
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-04-12 07:03:07 +0000

    fixed auth

--- v170/cgi-bin/webnotes/auth.py
+++ v170/cgi-bin/webnotes/auth.py
@@ -102,9 +102,9 @@ class HTTPRequest:
 				db_name = getattr(webnotes.defs,'db_name_map',{}).\
 					get(ac_name, ac_name)
 			else:
-				db_name = getattr(webnotes.defs,'default_db_name')
+				db_name = getattr(webnotes.defs,'default_db_name','')
 	
-		webnotes.conn = webnotes.db.Database(user = db_name,password = getattr(webnotes.defs,'db_password'))
+		webnotes.conn = webnotes.db.Database(user = db_name,password = getattr(webnotes.defs,'db_password',''))
 		webnotes.ac_name = ac_name
 
 # =================================================================================
