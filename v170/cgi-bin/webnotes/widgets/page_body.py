5b0eb29c2bba8a290e88816471e27920df8e8dfb
rmehta <rmehta@60a7a8e8-ba71-11de-b431-8336cfaced44>
2011-04-23 06:38:00 +0000

    page_body.py: missed out title- fixed

--- v170/cgi-bin/webnotes/widgets/page_body.py
+++ v170/cgi-bin/webnotes/widgets/page_body.py
@@ -132,7 +132,7 @@ def load_page_metatags(doc):
 	global page_properties
 
 	# page meta-tags
-	page_properties['page_title'] = doc.page_title or doc.name
+	page_properties['title'] = doc.page_title or doc.name
 	page_properties['keywords'] = doc.keywords or webnotes.conn.get_value('Control Panel',None,'keywords') or ''
 	page_properties['site_description'] = doc.site_description or webnotes.conn.get_value('Control Panel',None,'site_description') or ''
 	page_properties['add_in_head'] = webnotes.conn.get_global('add_in_head') or ''
